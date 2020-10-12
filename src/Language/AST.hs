07c437944bb2c30ece76ba3aaf24b8239f64d94d
Chris Phifer <cphifer@galois.com>
2020-10-12 16:58:04 -0500

    Some intial types / structures for a new version of the AST

--- src/Language/AST.hs
+++ src/Language/AST.hs
@@ -3,7 +3,7 @@ Module      : Language.AST
 Description : The LINK AST
 Copyright   : (c) Galois, Inc. 2020
 License     : N/A
-Maintainer  : chiw@galois.co
+Maintainer  : chiw@galois.com
 Stability   : experimental
 Portability : N/A
 
@@ -13,8 +13,37 @@ is the target of the parser defined in @Parser.y@.
 
 module Language.AST where
 
+import Data.Void
+
 import Math
 import Physics.Model
+
+-- | A complete LINK program, which consists of configuration, a (nonempty)
+--   collection of 'Physics.Model.Model's, and a collection of @Coupling@s.
+--
+--   Given @Prog config models couplings@, we have:
+--
+--   @length couplings = (length models * (length models - 1)) / 2@
+data Prog = Prog Config [Model] [Coupling]
+
+-- | A @Duration@ specifies how long a simulation should run, either as an
+--   explicit number of iterations or as an elapsed time.
+--   TODO: We need units, and probably shouldn't wait terribly long to do them
+data Duration = Iterations Int -- ^ The number of steps to take
+              | ElapsedTime Int -- ^ The amount of time to simulate
+
+-- | LINK Configuration, consisting of the global simulation step size and the
+--   total @Duration@
+data Config =
+  Config { getGlobalStep :: Int -- ^ The global solving step size TODO: This should have units
+         , getDuration :: Duration -- ^ The duration of the simulation, in time or in #iterations
+         }
+
+-- TODO: A coupling relates two models via boundary equations and knowledge of
+-- what variables are communicated via the boundary. At minimum, a coupling
+-- must consist of two models and the set of consistency/coordination equations
+-- that hold at the interface between the models.
+type Coupling = Void
 
 -- | A LINK program is a sequence of @Stmt@s
 newtype Decl = DStmts [Stmt]
