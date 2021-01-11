8b3b2b345543798ff9b7ba0f5cadb4e675027682
Chris Phifer <cphifer@galois.com>
2021-01-11 14:30:08 -0600

    Empty skeleton/module docs for alex/parsec interface

---
+++ src/Language/Token.hs
@@ -0,0 +1,18 @@
+{-|
+Module      : Language.Token
+Description : Interface between Alex lexer and Parsec parser
+Copyright   : Galois, Inc. 2021
+License     : N/A
+Maintainer  : cphifer@galois.com
+Stability   : Experimental
+Portability : N/A
+
+This module defines an interface between the lexer, generated using Alex, and
+Parsec, which is used to parse LINK programs.
+-}
+
+module Language.Token
+  (
+  ) where
+
+
