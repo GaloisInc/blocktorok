04254b9a9d902d13ac9ee1989f7ac9e173f867fd
Chris Phifer <cphifer@galois.com>
2021-01-11 16:07:05 -0600

    Basic top-level parsers that invoke the alex lexer

--- src/Language/Parser.hs
+++ src/Language/Parser.hs
@@ -15,6 +15,22 @@ module Language.Parser
   ) where
 
 import Language.AST
+import Language.Lexer
+import Language.Token
+
+import Text.Parsec
+
+parseText :: Parser a -> String -> Either ParseError a
+parseText p = parseNamedText p "<string>"
+
+parseNamedText :: Parser a -> String -> String -> Either ParseError a
+parseNamedText p n s =
+  case llex s of
+    Left e -> parse (reportLexError e) n []
+    Right xs -> parse p n xs
+
+reportLexError :: String -> Parser a
+reportLexError msg = fail ("lexical error: " ++ msg)
 
 parseDecl :: FilePath -> String -> Either String Prog
 parseDecl = undefined
