ef2f09fc0b3674775e5a71fcf3cccb656c190d66
Chris Phifer <cphifer@galois.com>
2021-01-19 15:23:09 -0600

    Skeleton of SU2 backend representation

---
+++ src/Data/Backends/SU2.hs
@@ -0,0 +1,15 @@
+{-|
+Module      : Data.Backends.SU2
+Description : Internal representation of SU2 configuration scripts
+Copyright   : (c) Galois, Inc. 2021
+License     : N/A
+Maintainer  : cphifer@galois.com
+Stability   : experimental
+Portability : N/A
+-}
+
+module Data.Backends.SU2
+  (
+  ) where
+
+data SU2Config
