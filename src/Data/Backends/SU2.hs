d5cd88af5d7141c362cf8b344d6d5761300a954c
Chris Phifer <cphifer@galois.com>
2021-01-19 16:32:31 -0600

    Beginnings of SU2 representation to support heat equation example

--- src/Data/Backends/SU2.hs
+++ src/Data/Backends/SU2.hs
@@ -9,7 +9,54 @@ Portability : N/A
 -}
 
 module Data.Backends.SU2
-  (
+  ( SU2Config(..)
   ) where
 
-data SU2Config
+data SU2Solver = Euler
+               | NS
+               | Rans
+               | EulerInc
+               | NSInc
+               | RansInc
+               | EulerNemo
+               | NSNemo
+               | EulerFEM
+               | NSFEM
+               | RansFEM
+               | LesFEM
+               | HeatFVM
+               | Elasticity
+
+data Objective = Drag
+               | Lift
+               | Sideforce
+               | XMoment
+               | YMoment
+               | ZMoment
+               | Efficiency
+               | EquivArea
+               | NearPressure
+               | XForce
+               | YForce
+               | ZForce
+               | Thrust
+               | Torque
+               | TotalHeatFlux
+               | MaxHeatFlux
+               | InvDesPressure
+               | InvDesHeatFlux
+               | SurfTotPressure
+               | SurfMassFlow
+               | SurfStatPressure
+               | SurfMach
+
+data SU2Config =
+  SU2Config { getSolver :: SU2Solver
+            , getObjectiveFn :: Objective -- TODO: Support multiple
+            , getObjectiveWt :: Double -- TODO: Support multiple
+            , isTimeDomain :: Bool
+            , getTimeStep :: Double -- TODO: Units?
+            , getMaxTime :: Double -- TODO: Units?
+            , getInternalIter :: Integer
+            , getTimeIter :: Integer
+            }
