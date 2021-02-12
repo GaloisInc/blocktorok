7026d227e6bcbe6514437217968a853e20e54ae9
Chris Phifer <cphifer@galois.com>
2021-02-12 16:47:07 -0600

    Formatting changes

--- Main.hs
+++ Main.hs
@@ -4,7 +4,7 @@ import Data.Class.Render
 import Language.Check (hasAllCouplings, allVarsDeclared)
 import Language.Compile.SU2 (compile)
 import Options
-import Text.Parse.Link ( parseDecl )
+import Text.Parse.Link (parseDecl)
 
 import Options.Applicative
 
@@ -22,9 +22,10 @@ realMain (Options input output) =
   do parseRes <- parseDecl input <$> readFile input
      case parseRes of
        Left e -> print e >> exitFailure
-       Right prog -> if hasAllCouplings prog && allVarsDeclared prog then
-                       case compile prog of
-                         Left e -> print e >> exitFailure
-                         Right su2 -> writeFile output (render su2)
-                     else
-                       error "Basic static analysis failed (do you have all couplings and are all variables declared before use?)"
+       Right prog ->
+         if hasAllCouplings prog && allVarsDeclared prog then
+           case compile prog of
+             Left e -> print e >> exitFailure
+             Right su2 -> writeFile output (render su2)
+         else
+           error "Basic static analysis failed (do you have all couplings and are all variables declared before use?)"
