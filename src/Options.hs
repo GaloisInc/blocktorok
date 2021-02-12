331031f6dd0d2805fdc7f258ddbd7605c5bef95a
Chris Phifer <cphifer@galois.com>
2021-02-12 13:23:01 -0600

    Skeleton for more robust command-line option handling

---
+++ src/Options.hs
@@ -0,0 +1,17 @@
+{-|
+Module:     : Options
+Description : Implementation of LINK command-line options
+Copyright   : (c) Galois, Inc. 2021
+Liense      : N/A
+Maintainer  : cphifer@galois.com
+Stability   : experimental
+Portability : N/A
+
+This module defines the command-line options available to the LINK compiler. It
+should be treated as ever-evolving as we determine our needs/wants/what is
+useful for demos etc.
+-}
+
+module Options
+  (
+  ) where
