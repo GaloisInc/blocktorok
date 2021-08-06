ddab137ec1029b002185c2eb756b890b0e2a2255
Chris Phifer <cphifer@galois.com>
2021-08-06 11:58:55 -0500

    Get rid of polymorphism in Factor definition

--- src/Language/Common/Units/Factor.hs
+++ src/Language/Common/Units/Factor.hs
@@ -13,17 +13,19 @@ mathematical manipulation.
 
 module Language.Common.Units.Factor where
 
+import Data.Text (Text)
+
 -- | A unit factor (unit & exponent)
-data Factor a = F a Integer deriving (Eq)
+data Factor = F Text Integer deriving (Eq)
 
 infix 4 $=
 -- | Return true iff the two given factors represent the same dimension
-($=) :: (Eq a) => Factor a -> Factor a -> Bool
+($=) :: Factor -> Factor -> Bool
 (F u1 _) $= (F u2 _) = u1 == u2
 
 -- | @extract s l@ pulls the 'Factor' matching @s@ out of @l@, returning the
 -- rest of the list and (if it exists) the extracted factor
-extract :: Eq a => Factor a -> [Factor a] -> ([Factor a], Maybe (Factor a))
+extract :: Factor -> [Factor] -> ([Factor], Maybe (Factor))
 extract _ []    = ([], Nothing)
 extract s (h:t) =
   if s $= h
@@ -32,7 +34,7 @@ extract s (h:t) =
 
 -- | @reorder fs1 fs2@ reorders the list of factors @fs1@ to match the order of
 -- @fs2@, putting anything not in @fs2@ at the end
-reorder :: Eq a => [Factor a] -> [Factor a] -> [Factor a]
+reorder :: [Factor] -> [Factor] -> [Factor]
 reorder fs1 [] = fs1
 reorder fs1 (h:t) =
   case extract h fs1 of
@@ -40,14 +42,14 @@ reorder fs1 (h:t) =
     (l, Just f) -> f : reorder l t
 
 -- | Remove all factors with exponent 0 from the given @[Factor]@
-normalize :: [Factor a] -> [Factor a]
+normalize :: [Factor] -> [Factor]
 normalize []          = []
 normalize (F _ 0 : t) = normalize t
 normalize (h : t)     = h : normalize t
 
 infixl 6 @@+
 -- | Add corresponding exponents, assuming similar ordering
-(@@+) :: Eq a => [Factor a] -> [Factor a] -> [Factor a]
+(@@+) :: [Factor] -> [Factor] -> [Factor]
 []             @@+ fs2                       = fs2
 fs1            @@+ []                        = fs1
 (F u1 e1 : t1) @@+ (F u2 e2 : t2) | u1 == u2 = F u1 (e1 + e2) : t1 @@+ t2
@@ -55,12 +57,12 @@ fs1            @@+ []                        = fs1
 
 infixl 6 @+
 -- | Add corresponding exponents, preserving order
-(@+) :: Eq a => [Factor a] -> [Factor a] -> [Factor a]
+(@+) :: [Factor] -> [Factor] -> [Factor]
 fs1 @+ fs2 = fs1 @@+ reorder fs2 fs1
 
 infixl 6 @@-
 -- | Subtract corresponding exponents, assuming similar ordering
-(@@-) :: Eq a => [Factor a] -> [Factor a] -> [Factor a]
+(@@-) :: [Factor] -> [Factor] -> [Factor]
 []             @@- fs2                       = negDim <$> fs2
 fs1            @@- []                        = fs1
 (F u1 e1 : t1) @@- (F u2 e2 : t2) | u1 == u2 = F u1 (e1 - e2) : t1 @@- t2
@@ -68,27 +70,27 @@ fs1            @@- []                        = fs1
 
 infixl 6 @-
 -- | Subtract corresponing exponents, preserving order
-(@-) :: Eq a => [Factor a] -> [Factor a] -> [Factor a]
+(@-) :: [Factor] -> [Factor] -> [Factor]
 fs1 @- fs2 | fs1 == fs2 = []
            | otherwise  = fs1 @@- reorder fs2 fs1
 
 infixl 7 @*
 -- Multiply the exponents in a @[Factor]@ by a scalar
-(@*) :: [Factor a] -> Integer -> [Factor a]
+(@*) :: [Factor] -> Integer -> [Factor]
 []          @* _     = []
 (F u e : t) @* power = F u (e * power) : (t @* power)
 
 infixl 7 @/
 -- Divide the exponents in a @[Factor]@ by a scalar
-(@/) :: [Factor a] -> Integer -> [Factor a]
+(@/) :: [Factor] -> Integer -> [Factor]
 []          @/ _     = []
 (F u e : t) @/ power = F u (e `div` power) : (t @/ power)
 
 infix 4 @~
 -- | Check if two @[Factor]@s should be considered equal
-(@~) :: Eq a => [Factor a] -> [Factor a] -> Bool
+(@~) :: [Factor] -> [Factor] -> Bool
 fs1 @~ fs2 = normalize (fs1 @- fs2) == []
 
 -- | Negate a single 'Factor'
-negDim :: Factor a -> Factor a
+negDim :: Factor -> Factor
 negDim (F u e) = F u (negate e)
