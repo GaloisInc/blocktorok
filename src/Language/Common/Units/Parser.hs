9baae82700626be5a2c32e28c07f65e37f33b7af
Chris Phifer <cphifer@galois.com>
2021-08-10 12:08:37 -0500

    Skeleton of unit parsing

---
+++ src/Language/Common/Units/Parser.hs
@@ -0,0 +1,13 @@
+{-|
+Module      : Language.Common.Units.Parser
+Description : Parsing of SI units
+Copyright   : (c) Galois, Inc. 2021
+License     : N/A
+Maintainer  : cphifer@galois.com
+Stability   : experimental
+Portability : N/A
+
+Parsers for units to be used in the schema/transformer languages.
+-}
+
+module Language.Common.Units.Parser where
