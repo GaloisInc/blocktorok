344d549430e51869903df235336a448648f9824b
Chris Phifer <cphifer@galois.com>
2021-09-07 12:28:29 -0500

    Add some modules for pretty document/data template generation

---
+++ Language/Schema/DocGen.hs
@@ -0,0 +1,17 @@
+{-|
+Module      : Language.Schema.DocGen
+Description : Markdown documentation generation from schemas
+Copyright   : (c) Galois, Inc. 2021
+License     : N/A
+Maintainer  : cphifer@galois.com
+Stability   : experimental
+Portability : N/A
+
+Generation of pretty-printed Markdown documentation from a Blocktorok schema.
+This is an experimental feature that will certainly require tuning to make sure
+the generated documentation is clear and well laid-out.
+-}
+
+module Language.Schema.DocGen
+  (
+  ) where
---
+++ Language/Schema/TemplateGen.hs
@@ -0,0 +1,17 @@
+{-|
+Module      : Language.Schema.TemplateGen
+Description : Blocktorok data template generation from schemas
+Copyright   : (c) Galois, Inc. 2021
+License     : N/A
+Maintainer  : cphifer@galois.com
+Stability   : experimental
+Portability : N/A
+
+Generation of pretty-printed Blocktorok data templates from a Blocktorok
+schema. This is an experimental feature that will certainly require tuning to
+make sure the generated template is correct.
+-}
+
+module Language.Schema.TemplateGen
+  (
+  ) where
