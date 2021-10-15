d96d175bc2985b8155b8624b99421af3ee1229c7
Chris Phifer <cphifer@galois.com>
2021-10-15 15:16:02 -0500

    Get rid of all the old examples and drafts

---
+++ test_cases/run_test.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+TCPATH=$(dirname "$BASH_SOURCE")
+
+pushd "$TCPATH/.." > /dev/null
+cabal build
+BLOCKTOROK=$(cabal exec -- which blocktorok)
+popd > /dev/null
+
+FAILURE_COUNT=0
+FAILURES=""
+
+for TEST in "$@"
+do
+  if [ -d $TEST ]; then
+    OUT="output"
+    pushd $TEST > /dev/null && \
+      rm -rf $OUT && \
+      mkdir -p expected && \
+      $BLOCKTOROK build -t transform.oct -o $OUT input.blok &&  \
+      diff -r -q expected $OUT
+    if [ $? -ne 0 ]; then
+      FAILURE_COUNT=$(($FAILURE_COUNT + 1))
+      FAILURES="$FAILURES\n$TEST"
+      echo -e "[\u001b[31m FAIL \u001b[0m] $TEST"
+    else
+      echo -e "[\u001b[32m OK   \u001b[0m] $TEST"
+    fi
+    popd > /dev/null
+  fi
+done
+
+echo "Failures: $FAILURE_COUNT"
+echo "Tests Failed:"
+echo -e $FAILURES
